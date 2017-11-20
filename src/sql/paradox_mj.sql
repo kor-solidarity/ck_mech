23b0ca0e7ca8bf4c910f0a3286f1275746d6b8fe
kor-solidarity <confed00@naver.com>
2017-11-20 18:36:04 +0900

    .

--- src/sql/paradox_mj.sql
+++ src/sql/paradox_mj.sql
@@ -1,37 +1,38 @@
 -- --------------------------------------------------------
 -- 호스트:                          127.0.0.1
--- 서버 버전:                        10.1.18-MariaDB - mariadb.org binary distribution
+-- 서버 버전:                        10.1.22-MariaDB - mariadb.org binary distribution
 -- 서버 OS:                        Win64
--- HeidiSQL 버전:                  9.3.0.4984
+-- HeidiSQL 버전:                  9.4.0.5125
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
 -- paradox_mj 데이터베이스 구조 내보내기
+DROP DATABASE IF EXISTS `paradox_mj`;
 CREATE DATABASE IF NOT EXISTS `paradox_mj` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `paradox_mj`;
 
-
 -- 테이블 paradox_mj.vic_factory 구조 내보내기
+DROP TABLE IF EXISTS `vic_factory`;
 CREATE TABLE IF NOT EXISTS `vic_factory` (
-  `fac_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'base num',
+  `ind_num` int(11) NOT NULL AUTO_INCREMENT,
   `reg_num` int(11) NOT NULL DEFAULT '0',
-  `fac_res` int(11) NOT NULL DEFAULT '0',
-  `fac_lvl` int(11) NOT NULL DEFAULT '0' COMMENT 'determines the size',
-  `열 5` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`fac_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='공장관련';
+  `ind_type` int(11) NOT NULL DEFAULT '0' COMMENT 'what do you make',
+  PRIMARY KEY (`ind_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 테이블 데이터 paradox_mj.vic_factory:~0 rows (대략적) 내보내기
 DELETE FROM `vic_factory`;
 /*!40000 ALTER TABLE `vic_factory` DISABLE KEYS */;
 /*!40000 ALTER TABLE `vic_factory` ENABLE KEYS */;
 
-
 -- 테이블 paradox_mj.vic_market 구조 내보내기
+DROP TABLE IF EXISTS `vic_market`;
 CREATE TABLE IF NOT EXISTS `vic_market` (
   `열 1` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='시장. 원래라면 지역별 고유시장이 존재하지만 현재는 빅1 시스템을 따른다. ';
@@ -41,8 +42,8 @@ DELETE FROM `vic_market`;
 /*!40000 ALTER TABLE `vic_market` DISABLE KEYS */;
 /*!40000 ALTER TABLE `vic_market` ENABLE KEYS */;
 
-
 -- 테이블 paradox_mj.vic_party 구조 내보내기
+DROP TABLE IF EXISTS `vic_party`;
 CREATE TABLE IF NOT EXISTS `vic_party` (
   `party_id` int(11) NOT NULL AUTO_INCREMENT,
   `party_name` varchar(50) NOT NULL,
@@ -60,21 +61,21 @@ DELETE FROM `vic_party`;
 /*!40000 ALTER TABLE `vic_party` DISABLE KEYS */;
 /*!40000 ALTER TABLE `vic_party` ENABLE KEYS */;
 
-
 -- 테이블 paradox_mj.vic_pop 구조 내보내기
+DROP TABLE IF EXISTS `vic_pop`;
 CREATE TABLE IF NOT EXISTS `vic_pop` (
   `pop_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'pop id',
   `pop_province` int(11) NOT NULL COMMENT 'province id',
   `pop_size` int(11) NOT NULL COMMENT 'actual ''population''',
   `pop_type` int(11) NOT NULL,
-  `pop_cult` int(11) NOT NULL,
+  `pop_cult` int(11) NOT NULL COMMENT 'culture',
   `pop_relig` int(11) NOT NULL,
-  `pop_mil` int(11) NOT NULL COMMENT 'militancy',
-  `pop_con` int(11) NOT NULL,
-  `pop_employ` int(11) NOT NULL COMMENT 'ppl employed',
-  `pop_cash` int(11) NOT NULL,
-  `pop_rebel` int(11) DEFAULT NULL COMMENT 'affiliated rebel faction if there is one',
-  `pop_lit` int(11) NOT NULL COMMENT 'literacy',
+  `pop_mil` int(11) NOT NULL DEFAULT '0' COMMENT 'militancy',
+  `pop_con` int(11) NOT NULL DEFAULT '0',
+  `pop_employ` int(11) NOT NULL DEFAULT '0' COMMENT 'employed num. may not be needed here',
+  `pop_cash` int(11) NOT NULL DEFAULT '0',
+  `pop_rebel` int(11) NOT NULL DEFAULT '0' COMMENT 'affiliated rebel faction if there is one',
+  `pop_lit` int(11) NOT NULL DEFAULT '30' COMMENT 'literacy',
   PRIMARY KEY (`pop_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -83,39 +84,41 @@ DELETE FROM `vic_pop`;
 /*!40000 ALTER TABLE `vic_pop` DISABLE KEYS */;
 /*!40000 ALTER TABLE `vic_pop` ENABLE KEYS */;
 
-
 -- 테이블 paradox_mj.vic_province 구조 내보내기
+DROP TABLE IF EXISTS `vic_province`;
 CREATE TABLE IF NOT EXISTS `vic_province` (
   `prov_num` int(11) NOT NULL AUTO_INCREMENT,
   `prov_name` varchar(50) NOT NULL,
   `prov_owner` int(11) DEFAULT NULL,
-  `prov_core` varchar(50) DEFAULT NULL,
-  `prov_res` int(11) NOT NULL COMMENT 'resource - number',
+  `prov_core` varchar(50) DEFAULT NULL COMMENT '어느 국가의 코어?',
+  `prov_res` int(11) NOT NULL COMMENT 'resource',
   `prov_rate` int(11) NOT NULL COMMENT 'life rating',
   `prov_region` int(11) NOT NULL,
+  `prov_corrupt` int(11) DEFAULT NULL COMMENT '부패',
   PRIMARY KEY (`prov_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='게임상 땅 한칸';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 테이블 데이터 paradox_mj.vic_province:~0 rows (대략적) 내보내기
 DELETE FROM `vic_province`;
 /*!40000 ALTER TABLE `vic_province` DISABLE KEYS */;
 /*!40000 ALTER TABLE `vic_province` ENABLE KEYS */;
 
-
 -- 테이블 paradox_mj.vic_region 구조 내보내기
+DROP TABLE IF EXISTS `vic_region`;
 CREATE TABLE IF NOT EXISTS `vic_region` (
   `reg_num` int(11) NOT NULL AUTO_INCREMENT,
   `reg_name` varchar(50) NOT NULL,
-  `reg_focus` varchar(50) NOT NULL COMMENT 'not really vic_friendly but for my sake',
-  `열 5` varchar(50) NOT NULL,
-  `열 6` varchar(50) NOT NULL,
+  `reg_focus` varchar(50) DEFAULT NULL COMMENT 'not really vic_friendly but for my sake',
+  `reg_plurality` int(11) NOT NULL DEFAULT '40' COMMENT '다양성',
+  `reg_ideo` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`reg_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='게임상 각 권역(주)';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 테이블 데이터 paradox_mj.vic_region:~0 rows (대략적) 내보내기
 DELETE FROM `vic_region`;
 /*!40000 ALTER TABLE `vic_region` DISABLE KEYS */;
 /*!40000 ALTER TABLE `vic_region` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/vic/vic_defs/constants.py
+++ src/vic/vic_defs/constants.py
@@ -1,1 +1,77 @@
-# poptypes
+# POP
+BASE_CLERGY_FOR_LITERACY = 0.005
+MAX_CLERGY_FOR_LITERACY = 0.04
+LITERACY_CHANGE_SPEED = 0.1
+
+ASSIMILATION_SCALE = 0.004
+CONVERSION_SCALE = 0.01
+IMMIGRATION_SCALE = 0.004
+
+PROMOTION_SCALE = 0.002
+PROMOTION_ASSIMILATION_CHANCE = 1.0
+LUXURY_THRESHOLD = 500
+BASE_GOODS_DEMAND = 0.8
+BASE_POPGROWTH = 0.0001
+MIN_LIFE_RATING_FOR_GROWTH = 30
+LIFE_RATING_GROWTH_BONUS = 0.0001
+LIFE_NEED_STARVATION_LIMIT = 0.5
+
+MIL_LACK_EVERYDAY_NEED = 0.1
+MIL_HAS_EVERYDAY_NEED = -0.1
+MIL_HAS_LUXURY_NEED = -0.2
+MIL_NO_LIFE_NEED = 0.2
+MIL_REQUIRE_REFORM = 0.2
+MIL_IDEOLOGY = -0.1
+MIL_RULING_PARTY = -0.1
+MIL_REFORM_IMPACT = 2
+MIL_WAR_EXHAUSTION = 0.005
+MIL_NON_ACCEPTED = 0.05
+
+CON_LITERACY = 0.1
+CON_LUXURY_GOODS = 0.1
+CON_POOR_CLERGY = -2.5
+CON_MIDRICH_CLERGY = -1.25
+CON_REFORM_IMPACT = -50
+CON_COLONIAL_FACTOR = 0.5
+RULING_PARTY_HAPPY_CHANGE = -1
+RULING_PARTY_ANGRY_CHANGE = 2
+
+PDEF_BASE_CON = 20.0  # so half'ed effect.
+
+NATIONAL_FOCUS_DIVIDER = 400000.0
+
+POP_SAVINGS = 0.018
+
+STATE_CREATION_ADMIN_LIMIT = 0.01
+
+MIL_TO_JOIN_REBEL = 7  # Rebels over this will join a faction
+MIL_TO_JOIN_RISING = 8  # Rebels over this will join a general rising
+MIL_TO_AUTORISE = 9  # Rebels over this rise no matter what
+REDUCTION_AFTER_RISEING = 0.0  # After a pop spawns a rebellion its militancy will be reduced this much
+
+# After a rebellion is being defeated in combat its pop militancy will be divided by this number.
+# (if value < 1.0 the MIL will be increased) (Beware! value must be > 0)
+REDUCTION_AFTER_DEFEAT = 3.0
+
+POP_TO_LEADERSHIP = 0.0001  # how much leadership every 1000 officers gives each day.
+ARTISAN_MIN_PRODUCTIVITY = 1  # Minimum efficieny of an artisan
+SLAVE_GROWTH_DIVISOR = 10  # Slaves have N times lower growth
+
+MIL_HIT_FROM_CONQUEST = 4  # how much militancy grows in a province if taken without being core.
+LUXURY_CON_CHANGE = 0.001  # con boost from over-buying luxuary goods
+INVENTION_IMPACT_ON_DEMAND = 0.005  # how much each invention in a country increases demand for a product in percent
+ARTISAN_SUPPRESSED_COLONIAL_GOODS_CATEGORY = 0  # Goods category index not produced in colonies
+ISSUE_MOVEMENT_JOIN_LIMIT = 8
+ISSUE_MOVEMENT_LEAVE_LIMIT = 7
+MOVEMENT_CON_FACTOR = 0.05
+MOVEMENT_LIT_FACTOR = 0.3
+MIL_ON_REB_MOVE = 8
+
+# controls base pop size for factor of supression/radicalness cost. zero  disables the feature and jsut uses radicalness
+POPULATION_SUPPRESSION_FACTOR = 0.0
+POPULATION_MOVEMENT_RADICAL_FACTOR = 300
+NATIONALIST_MOVEMENT_MIL_CAP = 3.0
+MOVEMENT_SUPPORT_UH_FACTOR = 3  # 3x means 30% country support equals full UH support
+REBEL_OCCUPATION_STRENGTH_BONUS = 0.01  # the amount of strength given to rebel movements when they occupy a province
+LARGE_POPULATION_LIMIT = 9000000
+LARGE_POPULATION_INFLUENCE_PENALTY_CHUNK = 250000
